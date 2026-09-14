9c68b4929ab035ee4861fc7f4c8a2a6d83e84bbd
Leny Peris <lenyperis@hotmail.fr>
2026-09-14 17:57:04 +0200

    ProjectileDamage and ennnemy freed on hit

--- galactic-bouillabaisse/Script/asteroid.gd
+++ galactic-bouillabaisse/Script/asteroid.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 @export var speed: float = 150
+@export var hp: int = 3
 #@onready var player = get_tree$"../Planet"
 var pos = Vector2(0,0)
 
@@ -8,8 +9,16 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var direction = global_position.direction_to(pos).normalized()
-	velocity = direction * speed
-	move_and_slide()
+	pass
+	#var direction = global_position.direction_to(pos).normalized()
+	#velocity = direction * speed
+	#move_and_slide()
+
+func hit(dmg : int) -> void:
+	hp -= dmg
+	print(hp)
+	if (hp <= 0):
+		queue_free()
