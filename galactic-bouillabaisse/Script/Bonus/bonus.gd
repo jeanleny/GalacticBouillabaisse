070984ca9bac475b9b08d603f9af60f52579ebd5
Tom Bourgeon <tobourge@1B6.42angouleme.fr>
2026-09-18 21:10:18 +0200

    Bonus Spawn

---
+++ galactic-bouillabaisse/Script/Bonus/bonus.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+@onready var sprite = $Sprite2D
+@onready var spriteBonus1 = preload("res://Asset/bonus/bonus1.png")
+@onready var spriteBonus2 = preload("res://Asset/bonus/bonus2.png")
+@onready var spriteBonus3 = preload("res://Asset/bonus/bonus3.png")
+
+@export var direction: Vector2
+@export var type: int #type est un int compris entre 0 et 99 genere au moment de l'instanciation
+#il permettra de gérer les probabilités d'apparition des différents bonus dans le jeu final
+
+func _ready():
+	if (type <= 33):
+		sprite.texture = spriteBonus1
+	elif (type <= 66):
+		sprite.texture = spriteBonus2
+	elif (type <= 99):
+		sprite.texture = spriteBonus3
+	
+	rotation = direction.angle()
+		
+	
--- galactic-bouillabaisse/Script/Enemies/asteroid.gd
+++ galactic-bouillabaisse/Script/Enemies/asteroid.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 @export var speed: float = 15
 @export var hp: int = 3
+@export var direction: Vector2
 #@onready var player = get_tree$"../Planet"
 var pos = Vector2(0,0)
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+@onready var BONUS_SCENE = preload("res://Scenes/Bonus/bonus.tscn")
+
+
 #@onready var sprite: Sprite2D = $Sprite2D
 
 # Called when the node entrs the scene tree for the first time.
@@ -15,7 +19,7 @@ func destroy() ->void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var direction = global_position.direction_to(pos).normalized()
+	direction = global_position.direction_to(pos).normalized()
 	velocity = direction * speed
 	move_and_slide()
 
@@ -23,3 +27,12 @@ func hit(dmg : int) -> void:
 	hp -= dmg
 	if (hp <= 0):
 		anim.play("destroy")
+		var rand = randi() % 200
+		if (rand <= 99): #Probabilite de generer un bonus (a modifier, 1 chance sur 2 pour le test)
+			generateBonus(rand)
+			
+func generateBonus(rand : int):
+	var bonus = BONUS_SCENE.instantiate()
+	bonus.type = rand
+	bonus.direction = direction
+	add_child(bonus)
